4e4c9e1a915fa49337c30e2eabd5935937ecd7a6
ebodenbender <embodenbender@gmail.com>
2021-11-28 15:36:44 -0800

    wired up buttons (just empty onClick methods as a placeholder for now)

--- app/src/main/java/com/bodenbender/emily/dungeonshare/MainActivity.java
+++ app/src/main/java/com/bodenbender/emily/dungeonshare/MainActivity.java
@@ -3,13 +3,40 @@ package com.bodenbender.emily.dungeonshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    public final String TAG = "MainActivityTag";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playDungeonButton = findViewById(R.id.playDungeonButton);
+        Button lookForDungeonButton = findViewById(R.id.lookForDungeonButton);
+        Button aboutAppButton = findViewById(R.id.aboutAppButton);
+
+        playDungeonButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+
+        lookForDungeonButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
     }
 }
