18f24907f16022c1a931164f5a462f7693bd0fbb
ebodenbender <embodenbender@gmail.com>
2021-11-30 17:08:30 -0800

    partially completed callbacks in broadcast class

--- app/src/main/java/com/bodenbender/emily/dungeonshare/Broadcast.java
+++ app/src/main/java/com/bodenbender/emily/dungeonshare/Broadcast.java
@@ -1,15 +1,26 @@
 package com.bodenbender.emily.dungeonshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
 
 public class Broadcast extends AppCompatActivity
 {
+    private ConnectionsClient connectionsClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,4 +34,54 @@ public class Broadcast extends AppCompatActivity
         AdvertisingOptions advertisingOptions = new AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build();
         //Nearby.getConnectionsClient(this).startAdvertising();
     }
+
+    private final PayloadCallback payloadCallback = new PayloadCallback()
+    {
+        @Override
+        public void onPayloadReceived(@NonNull String endpointId, @NonNull Payload payload)
+        {
+
+        }
+
+        @Override
+        public void onPayloadTransferUpdate(@NonNull String endpointId, @NonNull PayloadTransferUpdate payloadTransferUpdate)
+        {
+
+        }
+    };
+
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback = new EndpointDiscoveryCallback()
+    {
+        @Override
+        public void onEndpointFound(@NonNull String endpointId, @NonNull DiscoveredEndpointInfo discoveredEndpointInfo)
+        {
+            Nearby.getConnectionsClient(Broadcast.this)
+                    .requestConnection("requesting device", endpointId, connectionLifeCycleCallback);
+        }
+
+        @Override
+        public void onEndpointLost(@NonNull String endpointId)
+        {
+
+        }
+    };
+
+    private final ConnectionLifecycleCallback connectionLifecycleCallback = new ConnectionLifecycleCallback()
+    {
+        @Override
+        public void onConnectionInitiated(@NonNull String endpointId, @NonNull ConnectionInfo connectionInfo)
+        {
+            Nearby.getConnectionsClient(Broadcast.this).acceptConnection(endpointId, payloadCallback);
+        }
+
+        @Override
+        public void onConnectionResult(@NonNull String endpointId, @NonNull ConnectionResolution connectionResolution) {
+
+        }
+
+        @Override
+        public void onDisconnected(@NonNull String endpointId) {
+
+        }
+    };
 }
