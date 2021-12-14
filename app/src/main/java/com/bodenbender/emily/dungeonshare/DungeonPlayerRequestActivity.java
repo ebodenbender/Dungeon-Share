0ad263f8f654a0120237f4af60e81f532e25c7cf
ebodenbender <embodenbender@gmail.com>
2021-12-14 14:44:54 -0800

    added listener for players_in_dungeon to start DungeonPlayerDetailActivity once DM approves join request (or return to the list of available dungeons if the request is rejected)

--- app/src/main/java/com/bodenbender/emily/dungeonshare/DungeonPlayerRequestActivity.java
+++ app/src/main/java/com/bodenbender/emily/dungeonshare/DungeonPlayerRequestActivity.java
@@ -5,21 +5,28 @@
 
 package com.bodenbender.emily.dungeonshare;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DungeonPlayerRequestActivity extends AppCompatActivity
 {
-    static final String TAG = "PlayerRequestActivityTag";
+    static final String TAG = "PlayerRequestActivity";
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference playersInDungeonRef;
@@ -30,8 +37,12 @@ public class DungeonPlayerRequestActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dungeon_player_request);
 
+        Intent intent = getIntent();
+        String shareCode = intent.getStringExtra("shareCode");
+
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         playersInDungeonRef = mFirebaseDatabase.getReference("/players_in_dungeon");
+        playersInDungeonRef = playersInDungeonRef.child(shareCode);
 
         EditText playerNameEditText = findViewById(R.id.editTextPlayerName);
         EditText shareCodeEditText = findViewById(R.id.editTextShareCode);
@@ -50,13 +61,40 @@ public class DungeonPlayerRequestActivity extends AppCompatActivity
                 }
                 else
                 {
-                    Intent intent = getIntent();
-                    String shareCode = intent.getStringExtra("shareCode");
                     String playerName = playerNameEditText.getText().toString();
                     String inputShareCode = shareCodeEditText.getText().toString();
                     if (inputShareCode.equals(shareCode))
                     {
-                        playersInDungeonRef.child(inputShareCode).child(playerName).setValue(false);
+                        playersInDungeonRef.child(playerName).setValue(false);
+                        playersInDungeonRef.child(playerName).addValueEventListener(new ValueEventListener()
+                        {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot)
+                            {
+
+                                Boolean requestAccepted = snapshot.getValue(Boolean.class);
+                                if (requestAccepted != null)
+                                {
+                                    if (requestAccepted)
+                                    {
+                                        Intent intent = new Intent(DungeonPlayerRequestActivity.this, DungeonPlayerDetailActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                                else
+                                {
+                                    Toast.makeText(DungeonPlayerRequestActivity.this,
+                                            "Failed to join dungeon", Toast.LENGTH_SHORT).show();
+                                    DungeonPlayerRequestActivity.this.finish();
+                                    // TODO if player is removed from a dungeon after joining, they stay in the dungeon activity anyway
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
                     }
                     else
                     {
